b3d404141901f4db30f7a62bec8423e996e2229c
VladimirSemen2022 <107301979+VladimirSemen2022@users.noreply.github.com>
2023-07-10 23:29:11 +0300

    My form for UserControl and his bindings added

---
+++ View/PlantIcon.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UsefulPlantsCatalog.View
+{
+    /// <summary>
+    /// Логика взаимодействия для PlantIcon.xaml
+    /// </summary>
+    public partial class PlantIcon : UserControl
+    {
+        public static readonly DependencyProperty myDepPropertyFolk = DependencyProperty.Register(nameof(FolkName), typeof(string), typeof(UserControl));
+        public static readonly DependencyProperty myDepPropertyImg = DependencyProperty.Register(nameof(UrlPhoto), typeof(string), typeof(UserControl));
+        public static readonly DependencyProperty myDepPropertyScience = DependencyProperty.Register(nameof(ScienceName), typeof(string), typeof(UserControl));
+        public string FolkName
+        {
+            get { return (string)GetValue(myDepPropertyFolk); }
+            set { SetValue(myDepPropertyFolk, value); }
+        }
+
+        public string UrlPhoto
+        {
+            get { return (string)GetValue(myDepPropertyImg); }
+            set { SetValue(myDepPropertyImg, value); }
+        }
+
+        public string ScienceName
+        {
+            get { return (string)GetValue(myDepPropertyScience); }
+            set { SetValue(myDepPropertyScience, value); }
+        }
+        public PlantIcon()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+    }
+}
--- ViewModel/MainVM.cs
+++ ViewModel/MainVM.cs
@@ -1,19 +1,32 @@
-﻿using System;
+﻿using Pharmacy.Model;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Controls;
 using UsefulPlantsCatalog.Model;
+using UsefulPlantsCatalog.View;
 
 namespace UsefulPlantsCatalog.ViewModel
 {
     public class MainVM : INotifyPropertyChanged
     {
         private ObservableCollection<Plant> plants;
+        public List<PlantIcon> icons = new List<PlantIcon>();
         private Plant selectedPlant;
+        private PlantIcon selectedIcon;
+
+        public List<PlantIcon> Icons
+        {
+            get { return icons; }
+            set { icons = value; OnPropertyChanged("Icons"); }
+        }
+        public PlantIcon SelectedIcon
+        {
+            get { return selectedIcon; }
+            set { selectedIcon = value; OnPropertyChanged("SelectedIcon"); }
+        }
 
         public Plant SelectedPlant
         {
@@ -32,6 +45,26 @@ namespace UsefulPlantsCatalog.ViewModel
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public MainVM()
+        {
+            using (BDContext context = new BDContext())
+            {
+               Plants = new ObservableCollection<Plant>(context.CatalogOfPlants.ToList());
+                
+                foreach (var item in Plants)
+                    Icons.Add(new PlantIcon{ UrlPhoto = item.UrlPhoto ?? "", FolkName=item.FolkName, ScienceName=item.ScienceName ?? ""});
+              
+            }
+        }
+
+        public void AddItemsToWrapPanel()
+        {
+            
+            WrapPanel wrapPanel = new WrapPanel();
+            PlantIcon plantIcon = new PlantIcon();
+
         }
     }
 }
